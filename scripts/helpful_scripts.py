737c2b4ab40b8c5dd2a36304b91ab2e182f1750d
Codertjay <codertjay@gmail.com>
2022-02-17 23:25:59 +0100

    Initial commit

---
+++ scripts/helpful_scripts.py
@@ -0,0 +1,24 @@
+from brownie import network, accounts, config, MockV3Aggregator
+from web3 import Web3
+
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+LOCAL_BLOCKCHAIN_ENVIRONMENTS = ['development', 'ganache-local']
+
+
+def get_account():
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        return accounts[0]
+    else:
+        return accounts.add(config['wallets']['from_key'])
+
+
+def deploy_mocks():
+    account = get_account()
+    print('Deploying Mocks ...')
+    if len(MockV3Aggregator) <= 0:
+        MockV3Aggregator.deploy(
+            DECIMALS, Web3.toWei(STARTING_PRICE, 'ether'),
+            {'from': account}
+        )
+    print('Mocks Deployed.')
